0c14a9af8f546bded75ff014f8e4374cc831811c
Claus-Christoph Küthe <floss@vm01.telton.de>
2024-01-06 12:15:54 +0100

    Proper output using an output buffer
    Output so far was also done using STDOUT directly. Now we assume that STDOUT
    can be not ready at times and use stream_select to determine if it is ready too.
    However, this leads to the next problem: STDOUT is basically ready all the time.
    So we should only check if there is actually something to write.
    
    There is a conceptional bug - how to add the prompt "> " which should not have
    a new line? You'll see.

--- src/input/input-streamselect.php
+++ src/input/input-streamselect.php
@@ -6,6 +6,7 @@ class InputStreamSelect {
 	private int $count;
 	private bool $counting = false;
 	private int $last;
+	private array $writeBuffer = array();
 	function __construct() {
 		$this->lazy = new Lazy();
 	}
@@ -26,20 +27,20 @@ class InputStreamSelect {
 		}
 	}
 	
-	private function read(array $read){
+	private function read(array $read): void {
 		foreach($read as $value) {
 			$input = trim(fgets($value));
 			if($input === "") {
 				return;
 			}
 			if($input == "help") {
-				echo implode(PHP_EOL, $this->lazy->getHelp()).PHP_EOL;
+				$this->writeBuffer += $this->lazy->getHelp();
 			}
 			if($input == "date") {
-				echo implode(PHP_EOL, $this->lazy->getDate()).PHP_EOL;
+				$this->writeBuffer += $this->lazy->getDate();
 			}
 			if($input == "uptime") {
-				echo implode(PHP_EOL, $this->lazy->getUptime()).PHP_EOL;
+				$this->writeBuffer += $this->lazy->getUptime();
 			}
 			
 			#if($input == "count") {
@@ -54,6 +55,13 @@ class InputStreamSelect {
 		}
 	}
 	
+	public function write(array $write): void {
+		foreach($write as $value) {
+			$line = array_shift($this->writeBuffer);
+			fwrite($value, $line.PHP_EOL);
+		}
+	}
+	
 	function loop() {
 		echo "Enter 'help' for help!".PHP_EOL;
 		stream_set_blocking(STDIN, false);
@@ -61,11 +69,21 @@ class InputStreamSelect {
 			$read = array(STDIN);
 			$write = array();
 			$except = null;
-			#$write = array(STDOUT);
+			/*
+			 * write will be ready most of the time, defeating the purpose of
+			 * stream_select, as it would trigger most of the time. So we only
+			 * add STDIN to $write if there is something to be written.
+			 * NOTE: from another project I know that doing it wrong has a
+			 * measurable performance impact.
+			 */
+			if(count($this->writeBuffer)>0) {
+				$write = array(STDOUT);
+			}
 			if(stream_select($read, $write, $except, 0, 2000)<1) {
 				continue;
 			}
 			$this->read($read);
+			$this->write($write);
 		}
 	}
 }
