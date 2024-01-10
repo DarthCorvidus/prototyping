a6491b9fe46ac9315b34871a79db8902ac1a65a9
Claus-Christoph Küthe <floss@vm01.telton.de>
2024-01-10 17:58:12 +0100

    Example of using DirectoryLinear to count files & sum their sizes
    (sum of size is unreliable)

--- src/directory/directory-linear.php
+++ src/directory/directory-linear.php
@@ -1,8 +1,48 @@
 #!/usr/bin/env php
 <?php
 require __DIR__.'/../../vendor/autoload.php';
+class DirectorySize implements DirectoryObserver {
+	private int $size = 0;
+	private int $dirCount = 0;
+	private int $fileCount = 0;
+	public function onDirectory(\SplFileInfo $directory): void {
+		$this->size += $directory->getSize();
+	}
+
+	public function onFile(\SplFileInfo $file): void {
+		$this->fileCount++;
+		$this->size += ceil($file->getSize()/4096)*4096;
+	}
+	
+	public function onError(\RuntimeException $e): void {
+		echo $e->getMessage().PHP_EOL;
+	}
+
+	public function onLink(\SplFileInfo $link): void {
+		
+	}
+	
+	public function getFileCount(): int {
+		return $this->fileCount;
+	}
+	
+	public function getDirectoryCount(): int {
+		
+	}
+	
+	public function getSize(): int {
+		return $this->size;
+	}
+}
+
+
 \plibv4\profiler\Profiler::startTimer("linear");
+$do = new DirectorySize();
 $dir = new DirectoryLinear($argv[1]);
+$dir->addDirectoryObserver($do);
 $dir->run();
 \plibv4\profiler\Profiler::endTimer("linear");
 \plibv4\profiler\Profiler::printTimers();
+
+echo "Size: ".number_format($do->getSize()).PHP_EOL;
+echo "Files: ".number_format($do->getFileCount()).PHP_EOL;
