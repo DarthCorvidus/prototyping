d1a0f6d9b5610b1fc510615c5ad38098b25b0ae0
Claus-Christoph Küthe <floss@vm01.telton.de>
2024-01-13 15:33:43 +0100

    Forgot to add DirectorySize in last commit.

--- examples/directory/include/DirectorySize.php
+++ examples/directory/include/DirectorySize.php
@@ -3,7 +3,9 @@ class DirectorySize implements DirectoryObserver {
 	private int $size = 0;
 	private int $dirCount = 0;
 	private int $fileCount = 0;
+	private int $startTime;
 	public function onDirectory(\SplFileInfo $directory): void {
+		$this->dirCount++;
 		$this->size += $directory->getSize();
 	}
 
@@ -30,5 +32,16 @@ class DirectorySize implements DirectoryObserver {
 	
 	public function getSize(): int {
 		return $this->size;
+	}
+
+	public function onEnd(): void {
+		echo "Directories: ".$this->dirCount.PHP_EOL;
+		echo "Files:       ".$this->fileCount.PHP_EOL;
+		echo "Size:        ".$this->size.PHP_EOL;
+		echo "Time spent:  ".round((hrtime(true)-$this->startTime)/1000000000, 2).PHP_EOL;
+	}
+
+	public function onStart(): void {
+		$this->startTime = hrtime(true);
 	}
 }
