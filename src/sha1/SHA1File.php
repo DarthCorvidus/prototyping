0c6dc6a405a7d8a36efd9ca795f05c0cda916f70
Claus-Christoph Küthe <floss@vm01.telton.de>
2024-01-30 18:41:53 +0100

    Now implementing SHA1File is a walk in the park.

--- src/sha1/SHA1File.php
+++ src/sha1/SHA1File.php
@@ -1,10 +1,17 @@
 <?php
 class SHA1File extends SHA1 {
+	private SplFileObject $file;
 	public function __construct(\SplFileObject $file) {
-		parent::__construct($file);
+		$this->file = $file;
+		$this->length = $this->file->getSize();
+		$this->chunks = ($this->length>>6);
+		$mod = $this->length % 64;
+		if($mod >= 56) {
+			$this->overpad = true;
+		}
 	}
 	public function getData(int $chunk): string {
-		
+		return $this->file->fread(64);
 	}
 
 	public function start(): void {
---
+++ tests/sha1/SHA1FileTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+class SHA1FileTest extends TestCase {
+	function setUp(): void {
+		file_put_contents(__DIR__."/test.bin", random_bytes(1024*1024*10));
+	}
+	
+	function testGetHash() {
+		$expected = sha1_file(__DIR__."/test.bin");
+		$sha1 = new SHA1File(new SplFileObject(__DIR__."/test.bin"));
+		$hash = $sha1->getHash();
+		$this->assertEquals($expected, $hash);
+	}
+}
