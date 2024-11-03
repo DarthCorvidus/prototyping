144c044fcf8fe56659a537831f9fd0765b0fcffd
Claus-Christoph Küthe <github@vm01.telton.de>
2024-11-03 11:33:02 +0100

    Refactored ServerProcess to reflect changes in Timeshare classes.

--- examples/server/include/ServerProcess.php
+++ examples/server/include/ServerProcess.php
@@ -1,30 +1,34 @@
 <?php
 namespace Examples\Server;
-class ServerProcess implements \plibv4\process\Timeshared, \plibv4\process\TimeshareObserver {
+use \plibv4\process\Task;
+use \plibv4\process\TimeshareObserver;
+use \plibv4\process\Timeshare;
+use \plibv4\process\Scheduler;
+class ServerProcess implements Task, TimeshareObserver {
 	private mixed $server;
 	private mixed $clientId = 0;
-	private \plibv4\process\Timeshare $timeshare;
+	private \plibv4\process\Timeshare $sched;
 	private \plibv4\process\Timeshare $parent;
 	private $terminated = false;
 	private int $connected = 0;
-	function __construct(\plibv4\process\Timeshare $timeshare) {
+	function __construct(Scheduler $timeshare) {
 		$this->server = stream_socket_server("tcp://0.0.0.0:8000", $errno, $errstr);
-		$this->timeshare = new \plibv4\process\Timeshare();
+		$this->sched = new Timeshare();
 		$this->parent = $timeshare;
 	}
-	public function finish(): void {
+	public function __tsFinish(): void {
 		
 	}
 
-	public function kill(): void {
+	public function __tsKill(): void {
 		
 	}
 
-	public function loop(): bool {
+	public function __tsLoop(): bool {
 		if($this->terminated) {
-			return $this->timeshare->loop();
+			return $this->sched->loop();
 		}
-		$this->timeshare->loop();
+		$this->sched->loop();
 		$read = array($this->server);
 		$write = array();
 		if(stream_select($read, $write, $except, 0) < 1) {
@@ -32,9 +36,9 @@ class ServerProcess implements \plibv4\process\Timeshared, \plibv4\process\Times
 		}
 		$client = stream_socket_accept($this->server);
 		$clientStream = new StreamBinary($client, new \Examples\Server\Mode\Main());
-		$this->timeshare->addTimeshareObserver($this);
-		$this->timeshare->addTimeshared($clientStream);
-		echo "Client accepted, ".$this->timeshare->getProcessCount()." processes.".PHP_EOL;
+		$this->sched->addTimeshareObserver($this);
+		$this->sched->addTimeshared($clientStream);
+		echo "Client accepted, ".$this->sched->getProcessCount()." processes.".PHP_EOL;
 	return true;
 	}
 	
@@ -42,41 +46,57 @@ class ServerProcess implements \plibv4\process\Timeshared, \plibv4\process\Times
 		return $this->connected;
 	}
 	
-	public function pause(): void {
+	public function __tsPause(): void {
 		
 	}
 
-	public function resume(): void {
+	public function __tsResume(): void {
 		
 	}
 
-	public function start(): void {
+	public function __tsStart(): void {
 		
 	}
 
-	public function terminate(): bool {
+	public function __tsTerminate(): bool {
 		$this->terminated = true;
-		return $this->timeshare->terminate();
+		return $this->sched->terminate();
 	}
 
-	public function onAdd(\plibv4\process\Timeshare $timeshare, \plibv4\process\Timeshared $timeshared): void {
+	public function onAdd(Scheduler $sched, Task $task): void {
 		
 	}
 
-	public function onRemove(\plibv4\process\Timeshare $timeshare, \plibv4\process\Timeshared $timeshared, int $status): void {
-		if($timeshared instanceof \Examples\Server\StreamBinary) {
+	public function onRemove(Scheduler $sched, Task $task, int $status): void {
+		if($task instanceof \Examples\Server\StreamBinary) {
 			$this->connected--;
 			echo "Client disconnected, ".$this->connected." client(s).".PHP_EOL;
-			echo "Client processes: ".$this->timeshare->getProcessCount().PHP_EOL;
+			echo "Client processes: ".$this->sched->getProcessCount().PHP_EOL;
 		}
 	}
 
-	public function onStart(\plibv4\process\Timeshare $timeshare, \plibv4\process\Timeshared $timeshared): void {
-		if($timeshared instanceof \Examples\Server\StreamBinary) {
+	public function onStart(Scheduler $sched, Task $task): void {
+		if($task instanceof \Examples\Server\StreamBinary) {
 			$this->connected++;
 			$this->clientId++;
 			echo "Client accepted as ".$this->clientId.", ".$this->connected." client(s).".PHP_EOL;
-			echo "Client processes: ".$this->timeshare->getProcessCount().PHP_EOL;
+			echo "Client processes: ".$this->sched->getProcessCount().PHP_EOL;
 		}
+	}
+
+	public function __tsError(\Exception $e, int $step): void {
+		
+	}
+
+	public function onError(Scheduler $scheduler, Task $task, \Exception $e, int $step): void {
+		
+	}
+
+	public function onPause(Scheduler $scheduler, Task $task): void {
+		
+	}
+
+	public function onResume(Scheduler $scheduler, Task $task): void {
+		
 	}
 }
