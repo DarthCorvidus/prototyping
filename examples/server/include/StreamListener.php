76ca325b55b26fb54f487acabceda3305280f472
Claus-Christoph Küthe <github@vm01.telton.de>
2024-11-03 11:21:23 +0100

    Another change made some time ago.

--- examples/server/include/StreamListener.php
+++ examples/server/include/StreamListener.php
@@ -1,19 +1,6 @@
 <?php
 namespace Examples\Server;
 interface StreamListener {
-	function onData(string $data);
-	/*
-	 * hasData must not change the internal state, as it may be called by
-	 * Stream several times, if the stream is not ready to write.
-	 */
-	function hasData(): bool;
-	function getData(): string;
-	function onConnect();
-	function onDisconnect();
-	/**
-	 * To be called if Stream::terminate() was called.
-	 */
 	function onTerminate();
-	function loop(): bool;
-	function getBlocksize(): int;
+	function onDisconnect();
 }
